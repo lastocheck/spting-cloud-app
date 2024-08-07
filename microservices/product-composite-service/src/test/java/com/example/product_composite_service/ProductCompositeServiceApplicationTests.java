47697d6333f78eafb0a3241bf052541b8cf6db58
lastocheck <lastocheck@gmail.com>
2024-08-07 17:40:17 +0300

    Initial setup

---
+++ microservices/product-composite-service/src/test/java/com/example/product_composite_service/ProductCompositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.product_composite_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
