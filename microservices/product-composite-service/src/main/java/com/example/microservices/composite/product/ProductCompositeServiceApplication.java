aa2ed0a67eddaa13ea01e42f2a065d66ffd7c263
lastocheck <lastocheck@gmail.com>
2024-08-08 21:20:09 +0300

    Add integration class for product-composite-service
    Add integration class with methods to fetch base product, recommendation and review services

--- microservices/product-composite-service/src/main/java/com/example/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/com/example/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -2,11 +2,18 @@ package com.example.microservices.composite.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("com.example")
 public class ProductCompositeServiceApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
---
+++ microservices/product-composite-service/src/main/java/com/example/microservices/composite/product/services/ProductCompositeIntegration.java
@@ -0,0 +1,122 @@
+package com.example.microservices.composite.product.services;
+
+import com.example.api.core.product.Product;
+import com.example.api.core.product.ProductService;
+import com.example.api.core.recommendation.Recommendation;
+import com.example.api.core.recommendation.RecommendationService;
+import com.example.api.core.review.Review;
+import com.example.api.core.review.ReviewService;
+import com.example.api.exceptions.InvalidInputException;
+import com.example.api.exceptions.NotFoundException;
+import com.example.util.http.HttpErrorInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ProductCompositeIntegration implements ProductService, RecommendationService, ReviewService {
+    private static final Logger LOG = LoggerFactory.getLogger(ProductCompositeIntegration.class);
+    private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+
+    private final String productServiceUrl;
+    private final String recommendationServiceUrl;
+    private final String reviewServiceUrl;
+
+    @Autowired
+    public ProductCompositeIntegration(
+            RestTemplate restTemplate,
+            ObjectMapper objectMapper,
+            @Value("${app.product-service.host}") String productServiceHost,
+            @Value("${app.product-service.port}") int productServicePort,
+            @Value("${app.recommendation-service.host}") String recommendationServiceHost,
+            @Value("${app.recommendation-service.port}") int recommendationServicePort,
+            @Value("${app.review-service.host}") String reviewServiceHost,
+            @Value("${app.review-service.port}") int reviewServicePort
+    ) {
+        this.restTemplate = restTemplate;
+        this.objectMapper = objectMapper;
+
+        productServiceUrl = "http://%s:%d/product/".formatted(productServiceHost, productServicePort);
+        recommendationServiceUrl = "http://%s:%d/recommendation?productId=".formatted(recommendationServiceHost, recommendationServicePort);
+        reviewServiceUrl = "http://%s:%d/review?productId=".formatted(reviewServiceHost, reviewServicePort);
+    }
+
+    private String getErrorMessage(HttpClientErrorException ex) {
+        try {
+            return objectMapper.readValue(ex.getResponseBodyAsString(), HttpErrorInfo.class).getMessage();
+        } catch (IOException ioex) {
+            return ex.getMessage();
+        }
+    }
+
+    public Product getProduct(int productId) {
+        try {
+            String url = productServiceUrl + productId;
+
+            LOG.debug("Will call getProduct API on URL: {}", url);
+            Product product = restTemplate.getForObject(url, Product.class);
+            LOG.debug("Found a product with id: {}", product.getProductId());
+
+            return product;
+        } catch (HttpClientErrorException e) {
+            switch (HttpStatus.resolve(e.getStatusCode().value())) {
+                case NOT_FOUND -> throw new NotFoundException(getErrorMessage(e));
+                case UNPROCESSABLE_ENTITY -> throw new InvalidInputException(getErrorMessage(e));
+                default -> {
+                    LOG.warn("Got an unexpected HTTP error: {}, will rethrow it", e.getStatusCode());
+                    LOG.warn("Error body: {}", e.getResponseBodyAsString());
+                    throw e;
+                }
+            }
+        }
+    }
+
+    @Override
+    public List<Recommendation> getRecommendations(int productId) {
+        try {
+            String url = recommendationServiceUrl + productId;
+
+            LOG.debug("Will call getRecommendations API on URL: {}", url);
+            List<Recommendation> recommendations = restTemplate.
+                    exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Recommendation>>() {})
+                    .getBody();
+            LOG.debug("Found {} recommendations for a product with id: {}", recommendations.size(), productId);
+
+            return recommendations;
+        } catch (Exception e) {
+            LOG.warn("Got an exception while requesting recommendations, return zero recommendations: {}", e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    public List<Review> getReviews(int productId) {
+        try {
+            String url = reviewServiceUrl + productId;
+
+            LOG.debug("Will call getReviews API on URL: {}", url);
+            List<Review> reviews = restTemplate.
+                    exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<List<Review>>() {})
+                    .getBody();
+            LOG.debug("Found {} reviews for a product with id: {}", reviews.size(), productId);
+
+            return reviews;
+        } catch (Exception e) {
+            LOG.warn("Got an exception while requesting reviews, return zero reviews: {}", e.getMessage());
+            return new ArrayList<>();
+        }
+    }
+}
