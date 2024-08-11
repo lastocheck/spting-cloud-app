8fe0f6fe2ceab0a981104d6d297d27d1881d77cd
lastocheck <lastocheck@gmail.com>
2024-08-11 12:13:34 +0300

    Update review service to use non-blocking model

--- microservices/review-service/src/main/java/com/example/microservices/core/review/ReviewServiceApplication.java
+++ microservices/review-service/src/main/java/com/example/microservices/core/review/ReviewServiceApplication.java
@@ -2,14 +2,38 @@ package com.example.microservices.core.review;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
 
 @SpringBootApplication
 @ComponentScan("com.example")
 public class ReviewServiceApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(ReviewServiceApplication.class);
+
+	private final Integer threadPoolSize;
+	private final Integer taskQueueSize;
+
+	@Autowired
+	public ReviewServiceApplication(
+			@Value("${app.threadPoolSize:10}") Integer threadPoolSize,
+			@Value("${app.taskQueueSize:100}") Integer taskQueueSize
+	) {
+		this.threadPoolSize = threadPoolSize;
+		this.taskQueueSize = taskQueueSize;
+	}
+
+	@Bean
+	public Scheduler jdbcScheduler() {
+		LOG.info("Creates a jdbcScheduler with thread pool size = {}", threadPoolSize);
+		return Schedulers.newBoundedElastic(threadPoolSize, taskQueueSize, "jdbc-pool");
+	}
+
 
 	public static void main(String[] args) {
 		var context = SpringApplication.run(ReviewServiceApplication.class, args);
--- microservices/review-service/src/main/java/com/example/microservices/core/review/services/ReviewServiceImpl.java
+++ microservices/review-service/src/main/java/com/example/microservices/core/review/services/ReviewServiceImpl.java
@@ -8,10 +8,15 @@ import com.example.microservices.core.review.persistence.ReviewRepository;
 import com.example.util.http.ServiceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 
 import java.util.List;
+import java.util.logging.Level;
 
 @RestController
 public class ReviewServiceImpl implements ReviewService {
@@ -19,33 +24,50 @@ public class ReviewServiceImpl implements ReviewService {
     private final ServiceUtil serviceUtil;
     private final ReviewMapper mapper;
     private final ReviewRepository repository;
+    private final Scheduler jdbcScheduler;
 
-    public ReviewServiceImpl(ServiceUtil serviceUtil, ReviewMapper mapper, ReviewRepository repository) {
+    public ReviewServiceImpl(ServiceUtil serviceUtil, ReviewMapper mapper, ReviewRepository repository, @Qualifier("jdbcScheduler") Scheduler jdbcScheduler) {
         this.serviceUtil = serviceUtil;
         this.mapper = mapper;
         this.repository = repository;
+        this.jdbcScheduler = jdbcScheduler;
     }
 
     @Override
-    public List<Review> getReviews(int productId) {
+    public Flux<Review> getReviews(int productId) {
         LOG.debug("/review call to return reviews for productId={}", productId);
 
         if (productId < 1) {
             throw new InvalidInputException("Invalid productId: " + productId);
         }
 
+        return Mono.fromCallable(() -> internalGetReviews(productId))
+                .flatMapMany(Flux::fromIterable)
+                .log(LOG.getName(), Level.FINE)
+                .subscribeOn(jdbcScheduler);
+    }
+
+    private List<Review> internalGetReviews(int productId) {
         List<ReviewEntity> entityList = repository.findByProductId(productId);
-        List<Review> apis = mapper.entityListToApiList(entityList);
-        apis.forEach(e -> e.setServiceAddress(serviceUtil.getServiceAddress()));
+        List<Review> reviews = mapper.entityListToApiList(entityList);
+        reviews.forEach(e -> e.setServiceAddress(serviceUtil.getServiceAddress()));
 
-        LOG.debug("getReviews: return reviews of size {} for productId={}", apis.size(), productId);
-
-        return apis;
+        LOG.debug("getReviews: return reviews of size {} for productId={}", reviews.size(), productId);
+        return reviews;
     }
 
     @Override
-    public Review createReview(Review body) {
+    public Mono<Review> createReview(Review body) {
         LOG.debug("createReview: try to create review for productId: {}, reviewId: {}", body.getProductId(), body.getReviewId());
+
+        if (body.getProductId() < 1) {
+            throw new InvalidInputException("Invalid productId: " + body.getProductId());
+        }
+
+        return Mono.fromCallable(() -> internalCreateReview(body)).subscribeOn(jdbcScheduler);
+    }
+
+    private Review internalCreateReview(Review body) {
         try {
             ReviewEntity entity = mapper.apiToEntity(body);
             ReviewEntity newEntity = repository.save(entity);
@@ -59,7 +81,14 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
-    public void deleteReviews(int productId) {
+    public Mono<Void> deleteReviews(int productId) {
+        if (productId < 1) {
+            throw new InvalidInputException("Invalid productId: " + productId);
+        }
+        return Mono.fromRunnable(() -> internalDeleteReviews(productId)).subscribeOn(jdbcScheduler).then();
+    }
+
+    private void internalDeleteReviews(int productId) {
         LOG.debug("deleteReviews: tries to delete reviews for the product with productId: {}", productId);
         repository.deleteAll(repository.findByProductId(productId));
     }
