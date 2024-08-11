6719f237801453b64c0e7e85d87a1b686e1dcb77
lastocheck <lastocheck@gmail.com>
2024-08-11 11:17:32 +0300

    Change product and recommendation repositories to reactive

--- microservices/product-service/src/main/java/com/example/microservices/core/product/persistence/ProductRepository.java
+++ microservices/product-service/src/main/java/com/example/microservices/core/product/persistence/ProductRepository.java
@@ -1,10 +1,9 @@
 package com.example.microservices.core.product.persistence;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
-import java.util.Optional;
 
-public interface ProductRepository extends PagingAndSortingRepository<ProductEntity, String>, CrudRepository<ProductEntity, String> {
-    Optional<ProductEntity> findByProductId(int productId);
+public interface ProductRepository extends ReactiveCrudRepository<ProductEntity, String> {
+    Mono<ProductEntity> findByProductId(int productId);
 }
